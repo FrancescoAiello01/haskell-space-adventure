c4a918ee3b65593e0e1cf21fcf6de7c53aaa661c
Francesco Aiello <aiellof@oregonstate.edu>
2021-05-30 13:55:14 -0700

    Perform IO get the user's name and print to the screen

--- main.hs
+++ main.hs
@@ -1,1 +1,10 @@
-main = putStrLn "hello, world"
+main = do
+  putStrLn "Welcome to the Solar System!"
+  putStrLn "There are 9 planets to explore."
+  putStrLn "What is your name?"
+  name <- getLine
+  putStrLn ("Nice to meet you, " ++ name ++ ". My name is Eliza, I'm an old friend of Alexa.")
+  putStrLn "Let's go on an adventure!"
+  putStrLn "Shall I randomly  choose a planet for you to visit? (Y or N)"
+  planet <- getLine
+  putStrLn ("Going to fake planet")
