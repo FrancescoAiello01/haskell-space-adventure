66da5abbeb02e36d0e550747b6e06dd377dfe435
Francesco Aiello <aiellof@oregonstate.edu>
2021-05-30 15:34:08 -0700

    Create a map for the planets and descriptions

--- Planets.hs
+++ Planets.hs
@@ -1,5 +1,20 @@
 module Planets(chooseAPlanet) where
 
+import qualified Data.Map.Strict as Map
+import Data.Map (Map())
+
+planets :: Map String String
+planets = Map.fromList [
+  ("Mercury","A very hot planet, closest to the sun."),
+  ("Venus","It's very cloudy here!"),
+  ("Earth", "There is something very familiar about this planet."),
+  ("Mars", "Known as the red planet."),
+  ("Jupiter", "A gas giant, with a noticeable red spot."),
+  ("Saturn", "This planet has beautiful rings around it."),
+  ("Uranus", "Strangely, this planet rotates around on its side."),
+  ("Neptune", "A very cold planet, furthest from the sun."),
+  ("Pluto", "I don't care what they say - it's a planet.")]
+
 chooseAPlanet :: IO()
 chooseAPlanet = do
   putStrLn "Shall I randomly choose a planet for you to visit? (Y or N)"
