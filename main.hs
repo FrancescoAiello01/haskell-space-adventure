4e8733b67149d6a9251e62e919b8e984784f132c
Francesco Aiello <aiellof@oregonstate.edu>
2021-05-30 14:28:19 -0700

    Implement fake random planet using imperative approach

--- main.hs
+++ main.hs
@@ -6,5 +6,14 @@ main = do
   putStrLn ("Nice to meet you, " ++ name ++ ". My name is Eliza, I'm an old friend of Alexa.")
   putStrLn "Let's go on an adventure!"
   putStrLn "Shall I randomly  choose a planet for you to visit? (Y or N)"
-  planet <- getLine
-  putStrLn ("Going to fake planet")
+  choice <- getLine
+  if choice == "Y" then
+    putStrLn "Going to fake planet"
+  else if choice == "N" then
+    do
+      putStrLn "Name the planet you would like to visit."
+      planet <- getLine
+      putStrLn ("Traveling to " ++ planet ++ ". It is nice here.")
+  else
+    putStrLn "Sorry, I didn't get that."
+
