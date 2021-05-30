10b562d684b6aad2fd9ad477d7df1df241a9307c
Francesco Aiello <aiellof@oregonstate.edu>
2021-05-30 14:40:05 -0700

    Create a welcome message function

--- main.hs
+++ main.hs
@@ -1,10 +1,10 @@
-main = do
+welcomeMessage :: IO()
+welcomeMessage = do
   putStrLn "Welcome to the Solar System!"
   putStrLn "There are 9 planets to explore."
-  putStrLn "What is your name?"
-  name <- getLine
-  putStrLn ("Nice to meet you, " ++ name ++ ". My name is Eliza, I'm an old friend of Alexa.")
-  putStrLn "Let's go on an adventure!"
+
+main = do
+  welcomeMessage
   putStrLn "Shall I randomly  choose a planet for you to visit? (Y or N)"
   choice <- getLine
   if choice == "Y" then
