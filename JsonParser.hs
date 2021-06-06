c493979d95d55439de32b50d5c9383c294b5daf5
Francesco Aiello <aiellof@oregonstate.edu>
2021-06-06 11:30:31 -0700

    Find a planet by name (from json) and print at program start

--- JsonParser.hs
+++ JsonParser.hs
@@ -2,16 +2,17 @@
 module JsonParser (getData) where
 
 import Data.Aeson
-import Data.Text
+import Data.Text (Text)
 import Control.Applicative
 import Control.Monad
 import qualified Data.ByteString.Lazy as B
 import GHC.Generics
+import Data.List (findIndex, find)
 
--- | Type of each JSON entry in record syntax.
+-- Type of each JSON entry in record syntax
 data Planet =
-  Planet { name  :: !Text
-         , description   :: !Text
+  Planet { name  :: !String
+         , description   :: !String
            } deriving (Show,Generic)
 
 
@@ -26,13 +27,33 @@ getJSON :: IO B.ByteString
 getJSON = B.readFile jsonFile
 
 
+planetName :: Planet -> String
+planetName (Planet x _) = x
+
+planetDescription :: Planet -> String
+planetDescription (Planet _ x) = x
+
+mbPlanetName :: Maybe Planet -> Maybe String
+mbPlanetName (Just x) = Just (planetName x)
+mbPlanetName _ = Nothing
+
+mbPlanetDescription :: Maybe Planet -> Maybe String
+mbPlanetDescription (Just x) = Just (planetDescription x)
+mbPlanetDescription _ = Nothing
+
+
 getData = do
-  -- Get JSON data and decode it
   d <- (eitherDecode <$> getJSON) :: IO (Either String [Planet])
-  -- If d is Left, the JSON was malformed.
-  -- In that case, we print the error.
-  -- Otherwise, we perform the operation of
-  -- our choice. In this case, just print it.
+  -- If d is Left, there is an error with the data
   case d of
     Left err -> putStrLn err
-    Right ps -> print ps
+    Right ps -> do
+      mbPrint $ mbPlanetName $ find (("Pluto" ==) . planetName) ps
+      mbPrint $ mbPlanetDescription $ find (("Pluto" ==) . planetName) ps
+
+
+mbPrint
+  :: Show a
+  => Maybe a -> IO ()
+mbPrint (Just x) = print x
+mbPrint x = print x
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Haskell Space Adventure
 
 An exciting text-based adventure of the solar system, written in the Haskell programming language.
+
+Dependencies
+`
+aeson -- JSON parsing
+`
